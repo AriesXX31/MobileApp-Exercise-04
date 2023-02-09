967d34781a56eb6fb4ccbff14d03232cd55b288d
Renaldy Lawongan <s2200053@student.unklab.ac.id>
2023-02-09 11:44:34 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+/* Nama: Lawongan, Renaldy Richard - Exercise #04 */
+
+// No. 1
+// const helloWorld = () => {
+//   return new Promise((resolve, rejected) => {
+//     setTimeout(() => resolve("Hello World!"), 2000);
+//   });
+// };
+
+// const messages = async () => {
+//   const msg = await helloWorld();
+//   console.log(msg);
+// };
+
+// messages();
+
+// No. 2
+// const ambilDataUser = async () => {
+//   fetch("https://jsonplaceholder.typicode.com/users")
+//     .then((response) => response.json())
+//     .then((json) => console.log(json));
+// };
+
+// ambilDataUser();
+
+// No. 3
+// const ambilDataUser = async () => {
+//   const response = await fetch("https://jsonplaceholder.typicode.com/users");
+//   const json = await response.json();
+//   console.log(json);
+// };
+
+// ambilDataUser();
+
+// No. 4
+// const axios = require("axios");
+
+// const ambilDataUser = async () => {
+//   const response = await axios.get(
+//     "https://jsonplaceholder.typicode.com/users"
+//   );
+//   console.log(response.data);
+// };
+
+// ambilDataUser();
